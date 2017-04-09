5a1f3b7d7a703278b546aaf638eac1b2005011b1
Keleiron <johnathan.crocker@gmail.com>
2017-04-09 17:08:20 -0500

    Initial Commit

---
+++ blog/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from .models import Post
+
+
+class PostAdmin(admin.ModelAdmin):
+	"""The list display is used to display attributes of the model class in the admin model list"""
+	list_display = ('title', 'author', 'created_date', 'published_date')
+	"""Filters based on dates mostly it seems"""
+	list_filter = ['created_date', 'published_date',]
+	search_fields = ['title', 'text',]
+
+# Register your models here.
+admin.site.register(Post, PostAdmin)
