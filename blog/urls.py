b3d97c1c301da659521f9538505007fc7a606fba
Keleiron <johnathan.crocker@gmail.com>
2017-04-11 14:27:57 -0500

    Added ability to publish and unpublish, and can view drafts

--- blog/urls.py
+++ blog/urls.py
@@ -1,3 +1,4 @@
+"""Copyright Johnathan Crocker 2017"""
 from django.conf.urls import url
 from . import views
 
@@ -5,4 +6,8 @@ urlpatterns = [
 	url(r'^$', views.post_list, name='post_list'),
 	url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
 	url(r'^post/new/$', views.post_new, name='post_new'),
+	url(r'^post/edit/(?P<pk>\d+)/$', views.post_edit, name='post_edit'),
+	url(r'^drafts/$', views.post_draftList, name='post_draftList'),
+	url(r'^post/publish/(?P<pk>\d+)/$', views.post_publish, name='post_publish'),
+	url(r'^post/unpublish/(?P<pk>\d+)/$', views.post_unpublish, name='post_unpublish'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,4 @@
+"""Copyright Johnathan Crocker 2017"""
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Post
@@ -19,10 +20,37 @@ def post_new(request):
 		if form.is_valid():
 			post = form.save(commit=False)
 			post.author = request.user
-			post.published_date = timezone.now()
 			post.save()
 			return redirect('post_detail', pk=post.pk)
 	else:
 		form = PostForm()
 	
 	return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_publish(request, pk):
+	post = get_object_or_404(Post, pk=pk)
+	post.publish()
+	return redirect('post_detail', pk=post.pk)
+	
+def post_unpublish(request, pk):
+	post = get_object_or_404(Post, pk=pk)
+	post.unpublish()
+	return redirect('post_detail', pk=post.pk)
+	
+def post_edit(request, pk):
+	post = get_object_or_404(Post, pk=pk)
+	if request.method == "POST":
+		form = PostForm(request.POST, instance=post)
+		if form.is_valid():
+			post = form.save(commit=False)
+			post.author = request.user
+			post.save()
+			return redirect('post_detail', pk=post.pk)
+	else:
+		form = PostForm(instance=post)
+	return render(request, 'blog/post_edit.html', {'form': form})
+	
+def post_draftList(request):
+	posts = Post.objects.filter(published_date__isnull=True).order_by('-created_date')
+	return render(request, 'blog/post_draftList.html', {'posts': posts})
+	
