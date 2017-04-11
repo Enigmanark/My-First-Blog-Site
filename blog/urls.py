f95c27dd9b8c63f72a05d1df841609d16353b0ce
Keleiron <johnathan.crocker@gmail.com>
2017-04-11 15:35:47 -0500

    Changed the post control buttons around and updated the author and published date stuff

--- blog/urls.py
+++ blog/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
 	url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
 	url(r'^post/new/$', views.post_new, name='post_new'),
 	url(r'^post/edit/(?P<pk>\d+)/$', views.post_edit, name='post_edit'),
+	url(r'^post/remove/(?P<pk>\d+)/$', views.post_remove, name='post_remove'),
 	url(r'^drafts/$', views.post_draftList, name='post_draftList'),
 	url(r'^post/publish/(?P<pk>\d+)/$', views.post_publish, name='post_publish'),
 	url(r'^post/unpublish/(?P<pk>\d+)/$', views.post_unpublish, name='post_unpublish'),
--- blog/views.py
+++ blog/views.py
@@ -37,6 +37,11 @@ def post_unpublish(request, pk):
 	post.unpublish()
 	return redirect('post_detail', pk=post.pk)
 	
+def post_remove(request, pk):
+	post = get_object_or_404(Post, pk=pk)
+	post.delete()
+	return redirect('post_list')
+	
 def post_edit(request, pk):
 	post = get_object_or_404(Post, pk=pk)
 	if request.method == "POST":
